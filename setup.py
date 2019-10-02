54a8842b5a66715eb87d68a4bd606fca9ecda1bd
charity faith <charity.sotero@gmail.com>
2019-10-02 15:03:24 -0700

    init commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Use satellite images to tag elements of interest.',
+    author='Charity Faith Sotero',
+    license='MIT',
+)
